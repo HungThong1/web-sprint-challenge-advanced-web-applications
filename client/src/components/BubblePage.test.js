6acb6ae30127572d2fc12fe791acb177928b81f4
Hung <htemail80@gmail.com>
2020-11-13 08:59:44 -0800

    mvp

--- client/src/components/BubblePage.test.js
+++ client/src/components/BubblePage.test.js
@@ -1,7 +1,15 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import BubblePage from "./BubblePage";
-
+const newColors = [
+  { code: { hex: "#f0f8ff" }, color: "aliceblue", id: 1 },
+  { code: { hex: "#99ddbc" }, color: "limegreen", id: 2 },
+];
 test("Fetches data and renders the bubbles", () => {
   // Finish this test
+  mockFetchBubbles.mockResolvedValue(newColors);
+  render(<BubblePage />);
+
+  const bubbles = screen.findByText(/bubble/i);
+  expect(await bubbles).toBeInTheDocument();
 });
